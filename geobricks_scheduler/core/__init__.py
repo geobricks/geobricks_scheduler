88d5fa7c75415c42f0d403b296f3b8ad0eb87e5f
Kalimaha <guido.barbaglia@gmail.com>
2014-11-24 09:03:49 +0100

    Module structure.

--- geobricks_scheduler/core/__init__.py
+++ geobricks_scheduler/core/__init__.py
@@ -1,4 +1,4 @@
-__package__ = 'geobricks_scheduler'
+__package__ = 'core'
 __author__ = 'Barbaglia, Guido - Murzilli, Simone'
 __email__ = 'guido.barbaglia@gmail.com; simone.murzilli@gmail.com;'
 __license__ = 'GPL2'
